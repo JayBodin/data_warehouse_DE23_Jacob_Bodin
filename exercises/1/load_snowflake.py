372ede76f001e1d213d182512e072991b3a48e3a
Jacob Bodin <Jacob.bodin@5mail.com>
2024-09-02 21:16:15 +0200

    exercise 1 easy

--- exercises/1/load_snowflake.py
+++ exercises/1/load_snowflake.py
@@ -3,10 +3,9 @@ import pandas as pd
 from pathlib import Path
 import os
 
-
 @dlt.resource(write_disposition="append")
 def load_snowflake_resource(file_path: str, **kwargs):
-    df = pd.read_excel(file_path, **kwargs)
+    df = pd.read_csv(file_path, **kwargs)
     yield df.to_dict(orient="records")
 
 
@@ -14,10 +13,8 @@ if __name__ == "__main__":
 
     working_directory = Path(__file__).parent
     os.chdir(working_directory)
-    # specify the pipeline name, destination and dataset name when configuring pipeline,
-    # otherwise the defaults will be used that are derived from the current script name
     pipeline = dlt.pipeline(
-        pipeline_name='load_snowflake',
+        pipeline_name='load_snowflake_ifood',
         destination='snowflake',
         dataset_name='staging', #schema
     )
@@ -27,15 +24,14 @@ if __name__ == "__main__":
 
     data = list(
         load_snowflake_resource(
-            working_directory / "exercise_data" / "iFood.xlsx"
+            working_directory / "data" / "ifood_df.csv", encoding="latin1"
         )
     )
     # print the data yielded from resource
     print(data)
 
     # run the pipeline with your parameters
-    # load_info = pipeline.run(data, table_name="netflix")
+    load_info = pipeline.run(data, table_name="marketing_data")
 
     # pretty print the information on data that was loaded
-    # print(load_info)
-    
+    print(load_info)
